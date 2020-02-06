92f0f7c7eb41001518bbc19a6424eec35dc26eae
Stefan Gvozdenovic <gefa12@gmail.com>
2020-02-06 00:28:03 -0500

    add pytest test code

---
+++ test_roman_numerals.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import pytest
+from roman_numerals import convert
+
+def test_basics():
+    numbers = [(1, 'I'), (3, 'III'), (4, 'IV'), (27, 'XXVII'), (44, 'XLIV'),
+            (93, 'XCIII'), (141, 'CXLI'), (402, 'CDII'), (575, 'DLXXV'),
+            (1024, 'MXXIV'), (3000, 'MMM')]
+
+    for num in numbers:
+        assert (num[0] == convert(num[1]))
+
+    for num in numbers:
+        assert (num[1] == convert(num[0]))
